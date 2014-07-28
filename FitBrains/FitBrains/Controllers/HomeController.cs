d5c944648df4de7ba78b77c4f699a389cf6212b8
linzh <linzh@wicresoft.com>
2014-07-28 14:43:55 +0800

    main

---
+++ FitBrains/FitBrains/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FitBrains.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region 页面加载
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult WhyFitBrains()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
